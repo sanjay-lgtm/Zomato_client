7d5ffcc6993d4274900c02feeb9cf0fd2923d13c
sanjay <113435192+sanjay-lgtm@users.noreply.github.com>
2024-07-29 00:51:53 +0530

    add -AuthProvider

--- src/auth/Auth0ProviderWithNavigate.tsx
+++ src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,3 +1,4 @@
+import { useCreateMyUser } from "@/api/MyUserApi";
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
 import React from "react";
 
@@ -5,6 +6,7 @@ type Props = {
   children: React.ReactNode;
 };
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
+  const { createUser } = useCreateMyUser();
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
@@ -13,8 +15,10 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
     throw new Error("unable to initialize auth!");
   }
 
-  const onRedirectCallback = (appState?:AppState,user?:User) => {
-    console.log("USER",user);
+  const onRedirectCallback = (appState?: AppState, user?: User) => {
+    if (user?.sub && user?.email) {
+      createUser({ auth0Id: user.sub, email: user.email });
+    }
   };
   return (
     <Auth0Provider
