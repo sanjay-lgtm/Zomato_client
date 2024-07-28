f685fe4432ae05b657f3f95e032e60cdbae573ef
sanjay <113435192+sanjay-lgtm@users.noreply.github.com>
2024-07-29 00:18:39 +0530

    add -loginpage and logout in web and mobile nav

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,10 +1,19 @@
 import React from "react";
-import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from "./ui/sheet";
-import { Menu } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { CircleUserRound, Menu } from "lucide-react";
 import { Separator } from "./ui/separator";
 import { Button } from "./ui/button";
+import { useAuth0 } from "@auth0/auth0-react";
+import MobileNavLinks from "./MobileNavLinks";
 
 const MobileNav = () => {
+  const { loginWithRedirect, isAuthenticated, user } = useAuth0();
   return (
     <Sheet>
       <SheetTrigger>
@@ -12,11 +21,24 @@ const MobileNav = () => {
       </SheetTrigger>
       <SheetContent className="space-y-3">
         <SheetTitle>
-          <span>Welcome to MernEasts.com</span>
+          {isAuthenticated ? (
+            <span className="flex items-center font-bold gap-2">
+              <CircleUserRound className="text-orange-500" />
+              {user?.email}
+            </span>
+          ) : (
+            <span>Welcome to MernEasts.com</span>
+          )}
         </SheetTitle>
-        <Separator/>
-        <SheetDescription className="flex">
-            <Button className="flex-1 font-bold bg-orange-500">Log In</Button>
+        <Separator />
+        <SheetDescription className="flex flex-col gap-4">
+          {isAuthenticated ? (
+            <MobileNavLinks />
+          ) : (
+            <Button 
+            onClick={() => loginWithRedirect()}
+            className="flex-1 font-bold bg-orange-500">Log In</Button>
+          )}
         </SheetDescription>
       </SheetContent>
     </Sheet>
