3faf39babcb29d8a93ad679964b0e5e3d05962d9
sanjay <113435192+sanjay-lgtm@users.noreply.github.com>
2024-07-29 10:17:24 +0530

    customize auth functionality AND token access

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,16 +1,24 @@
-import { Navigate, Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import HomePage from "./pages/HomePage";
+import AuthCallbackPage from "./pages/AuthCallbackPage";
 
 const AppRoutes = () => {
-
-    return(
-        <Routes>
-            <Route path="/" element={<Layout><HomePage/></Layout>}/>
-            <Route path="/user-profile" element={<span>user profile page</span>}/>
-            <Route path="*" element={<Navigate to={'/'}/>}/>
-        </Routes>
-    )
-}
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <Layout>
+            <HomePage />
+          </Layout>
+        }
+      />
+      <Route path="/auth-callback" element={<AuthCallbackPage />} />
+      <Route path="/user-profile" element={<span>user profile page</span>} />
+      <Route path="*" element={<Navigate to={"/"} />} />
+    </Routes>
+  );
+};
 
 export default AppRoutes;
--- src/auth/Auth0ProviderWithNavigate.tsx
+++ src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,12 +1,13 @@
-import { useCreateMyUser } from "@/api/MyUserApi";
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
   children: React.ReactNode;
 };
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
-  const { createUser } = useCreateMyUser();
+  const navigate = useNavigate();
+
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
@@ -15,11 +16,10 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
     throw new Error("unable to initialize auth!");
   }
 
-  const onRedirectCallback = (appState?: AppState, user?: User) => {
-    if (user?.sub && user?.email) {
-      createUser({ auth0Id: user.sub, email: user.email });
-    }
+  const onRedirectCallback = async (appState?: AppState, user?: User) => {
+    navigate("/auth-callback");
   };
+
   return (
     <Auth0Provider
       domain={domain}
