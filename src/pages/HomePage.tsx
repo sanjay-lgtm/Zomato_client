c42f559601cc2367fad02fef08a09aa7e449ec0b
sanjay <113435192+sanjay-lgtm@users.noreply.github.com>
2024-08-02 16:51:20 +0530

    resolve bug home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,8 +2,12 @@ import landingImage from "../assets/landing.png";
 import appDownloadImage from "../assets/appDownload.png";
 import SearchBar, { SearchForm } from "@/components/SearchBar";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
 const HomePage = () => {
   const navigate = useNavigate();
+  const [searchQuery, setSearchQuery] = useState("");
+
   const handleSearchSubmit = (searchFormValues: SearchForm) => {
     navigate({
       pathname: `/search/${searchFormValues.searchQuery}`,
@@ -12,27 +16,28 @@ const HomePage = () => {
 
   return (
     <div className="flex flex-col gap-12">
-      <div className="md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
+      <div className="container mx-auto px-4 md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
         <h1 className="text-5xl font-bold tracking-tight text-orange-600">
-          Track into a takeway today
+          Track into a takeaway today
         </h1>
         <span className="text-xl">Food is just a click away!</span>
         <SearchBar
           placeHolder="Search by City or Town"
           onSubmit={handleSearchSubmit}
+          searchQuery={searchQuery} // Pass searchQuery
         />
       </div>
       <div className="grid md:grid-cols-2 gap-5">
-        <img src={landingImage} />
+        <img src={landingImage} alt="Landing" />
         <div className="flex flex-col items-center justify-center gap-4 text-center">
           <span className="font-bold text-3xl tracking-tighter">
             Order takeaway even faster!
           </span>
           <span>
-            Download the MernEats App for faster ordering and personalised
+            Download the MernEats App for faster ordering and personalized
             recommendations
           </span>
-          <img src={appDownloadImage} />
+          <img src={appDownloadImage} alt="App Download" />
         </div>
       </div>
     </div>
